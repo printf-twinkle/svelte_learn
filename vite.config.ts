5a30bbc6f48a00d7ca7fee534786cc7e9636238d
printf-twinkle <twinklechokostar@gmail.com>
2024-07-20 23:04:00 +0530

    fixed errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  define: {
+    'process.env.POSTGRES_URL': JSON.stringify(process.env.POSTGRES_URL)
+  }
 });
